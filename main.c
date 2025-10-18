94e15282cdcf7f5a9094ebc26b263bfa283bcbcf
Ítalo Botelho <fragabotelhoitalo@gmail.com>
2025-10-17 21:56:17 -0300

    Esboço de função - decimal_to_bin()

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int decimal_to_bin(int num)
+{
+    int resto, quoc, *v, cont=0;
+    
+    do
+    {
+        quoc = num / 2;
+        resto = num % 2;
+
+        
+    }while(quoc > 2);
+    
+}
