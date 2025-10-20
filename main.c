528d9704a87e915c8c4ccbc4e79be9cc3db6c3a1
Ítalo Botelho <fragabotelhoitalo@gmail.com>
2025-10-19 21:36:32 -0300

    fix: Refatoração da função decimal_to_bin(int num): código otimizado

--- main.c
+++ main.c
@@ -11,28 +11,38 @@ int bin_to_hex(int num);
 int hex_to_decimal(int num);
 int hex_to_bin(int num);
 
+#include <stdio.h>
+
+// A função agora é 'void', pois ela mesma vai imprimir o resultado
+// em vez de retornar um número.
 void decimal_to_bin(int num)
 {
-    int quoc=num, cont=0;
-    int resto, vetor[cont];
-    
-    do
-    {
-        cont++;
-        resto = quoc % 2;
-        quoc /= 2;
-        vetor[cont] = resto;
-    }while(quoc != 0);
-
-    for(int i=cont; i>0; i--)
-    {
-        printf("%d", vetor[i]);
+    if (num == 0) {
+        printf("Binario: 0\n");
+        return;
     }
+
+    int bits[64];
+    int cont = 0, quoc = num;
+    
+    while (quoc > 0)
+    {
+        bits[cont] = quoc % 2;
+        quoc /= 2;
+        cont++;
+    }
+
+    printf("Binario de %d é: ", num);
+    for (int i = cont - 1; i >= 0; i--)
+    {
+        printf("%d", bits[i]);
+    }
+    printf("\n");
 }
 
 int main()
 {
-    decimal_to_bin(800);
+    decimal_to_bin(0);
 
     return 0;
 }
