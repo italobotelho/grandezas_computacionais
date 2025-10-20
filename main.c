9f01c298f341cee4160246f38918e1e88447569e
Ítalo Botelho <fragabotelhoitalo@gmail.com>
2025-10-19 21:19:14 -0300

    feat: funcao decimal_to_bin(int num)

--- main.c
+++ main.c
@@ -1,15 +1,38 @@
+//Conversão de grandezas operacionais
+//Código criado para fins de aprendizagem
+
 #include <stdio.h>
 
-int decimal_to_bin(int num)
+// Cabeçalho das funções
+void decimal_to_bin(int num);
+int decimal_to_hex(int num);
+int bin_to_decimal(int num);
+int bin_to_hex(int num);
+int hex_to_decimal(int num);
+int hex_to_bin(int num);
+
+void decimal_to_bin(int num)
 {
-    int resto, quoc, *v, cont=0;
+    int quoc=num, cont=0;
+    int resto, vetor[cont];
     
     do
     {
-        quoc = num / 2;
-        resto = num % 2;
+        cont++;
+        resto = quoc % 2;
+        quoc /= 2;
+        vetor[cont] = resto;
+    }while(quoc != 0);
 
-        
-    }while(quoc > 2);
-    
+    for(int i=cont; i>0; i--)
+    {
+        printf("%d", vetor[i]);
+    }
+}
+
+int main()
+{
+    decimal_to_bin(800);
+
+    return 0;
 }
